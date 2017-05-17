bce2b05fb206808a55ae83bd4d8e4bd760c4b4da
deMoro <ddnnss.i1@gmail.com>
2017-05-17 22:55:31 +0300

    Initial commit

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,5 @@
+class PageController < ApplicationController
+  def index
+    @title = 'Станция технического обслуживания и ремона автомобилей всех марок - ЗАО ПФ Автостар | Главная'
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+require 'base64'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module AvtoStar
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('app','assets','fonts')
+    config.assets.paths << Rails.root.join('app','assets','images','menu')
+    config.assets.paths << Rails.root.join('app','assets','images','menu','splash')
+    config.assets.paths << Rails.root.join('app','assets','images','slider')
+    config.assets.paths << Rails.root.join('app','assets','images','homepage')
+    config.assets.paths << Rails.root.join('app','assets','images','homepage','capthca')
+    config.assets.paths << Rails.root.join('app','assets','images','pages')
+  end
+end
